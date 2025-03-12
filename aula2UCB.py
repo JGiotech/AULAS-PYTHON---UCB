ffe05705fe1c6a2b00bb119af08277007edce757
JGiotech <giovannabarbosah38@gmail.com>
2025-03-12 10:56:17 -0300

    atualização

--- aula2UCB.py
+++ aula2UCB.py
@@ -24,22 +24,50 @@
 # a /= 1
 # print (a)
 
-import math
+# import math
 
-print(round(2.9)) # FUNCÃO ROUND ARREDONDA NÚMEROS DECIMAIS
-print(math.pi) # FUNÇÃO MATH.PI RETORNA O VALOR DE PI
-print(abs(-2.9)) # FUNÇÃO abs RETORNA UM NUMERO ABSOLUTA (SINAL NÃO IMPORTA)
-print(pow(2, 3)) # FUNÇÃO pow RETORNA UMA EXPONENCIAÇÃO
-print(min(2, 3)) #NUMERO MINIMO ENTRE 2 E 3
-print(sum({1, 2, 3})) #RETORNA UMA SOMA
-print(chr(65)) #NUMERO DE UMA LETRA
-print(ord("A")) #qual numero é a letra
-print(divmod(10, 3)) #RETORNA O QUOCIENTE E O RESTO DA DIVISAO
-print(math.ceil(2.1)) #MAXIMO
-print(math.floor(2.9)) #MINIMO
+# print(round(2.9)) # FUNCÃO ROUND ARREDONDA NÚMEROS DECIMAIS
+# print(math.pi) # FUNÇÃO MATH.PI RETORNA O VALOR DE PI
+# print(abs(-2.9)) # FUNÇÃO abs RETORNA UM NUMERO ABSOLUTA (SINAL NÃO IMPORTA)
+# print(pow(2, 3)) # FUNÇÃO pow RETORNA UMA EXPONENCIAÇÃO
+# print(min(2, 3)) #NUMERO MINIMO ENTRE 2 E 3
+# print(sum({1, 2, 3})) #RETORNA UMA SOMA
+# print(chr(65)) #NUMERO DE UMA LETRA
+# print(ord("A")) #qual numero é a letra
+# print(divmod(10, 3)) #RETORNA O QUOCIENTE E O RESTO DA DIVISAO
+# print(math.ceil(2.1)) #MAXIMO
+# print(math.floor(2.9)) #MINIMO
 
 
+#x = input("Digite um numero:  ") #input só recebe string
 
+# x = int(input("Digite um número: ")) # conversão para int
+# print(type(x))
+
+# y = (x) + 1
+# print(y)
+
+# a = float(input("Digite um numero ponto flutuante: "))
+# print(type(a))
+
+# b = (a) + 4
+# print(b)
+
+
+# name = input("Digite seu primeiro nome: ")
+# name2 = input("Digite seu sobrenome: ")
+# full_name = f"{name} {name2}"
+
+# print(full_name)
+
+# idade = 20
+
+print (2 >1)
+print (2 >=1)
+print (2 <1)
+print (2 <=1)
+print (2 == 1)
+print (2 != 1)
 
 
 
