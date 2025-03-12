1b870a6b7b32c60405479d1045eef334aa594326
JGiotech <giovannabarbosah38@gmail.com>
2025-03-12 10:01:33 -0300

    Adicionando novos arquivos aula2UCB.py e hello.py

---
+++ aula2UCB.py
@@ -0,0 +1,47 @@
+# a = 20
+# b = 10
+
+# soma = a + b
+# subtracao = a - b
+# divisao = a / b
+# multiplicacao = a * b
+# exponenciacao = a ** b
+# divisao_sem_resto = a // b
+# resto_da_divisao = a % b
+
+# print(soma) 
+# print(subtracao) 
+# print(divisao)  
+# print(multiplicacao) 
+# print(exponenciacao) 
+# print(divisao_sem_resto)
+# print(resto_da_divisao) 
+
+# a += 1
+# print (a)
+# a -= 1
+# print (a)
+# a /= 1
+# print (a)
+
+import math
+
+print(round(2.9)) # FUNCÃO ROUND ARREDONDA NÚMEROS DECIMAIS
+print(math.pi) # FUNÇÃO MATH.PI RETORNA O VALOR DE PI
+print(abs(-2.9)) # FUNÇÃO abs RETORNA UM NUMERO ABSOLUTA (SINAL NÃO IMPORTA)
+print(pow(2, 3)) # FUNÇÃO pow RETORNA UMA EXPONENCIAÇÃO
+print(min(2, 3)) #NUMERO MINIMO ENTRE 2 E 3
+print(sum({1, 2, 3})) #RETORNA UMA SOMA
+print(chr(65)) #NUMERO DE UMA LETRA
+print(ord("A")) #qual numero é a letra
+print(divmod(10, 3)) #RETORNA O QUOCIENTE E O RESTO DA DIVISAO
+print(math.ceil(2.1)) #MAXIMO
+print(math.floor(2.9)) #MINIMO
+
+
+
+
+
+
+
+
---
+++ hello.py
@@ -0,0 +1,70 @@
+# # print("hello word")
+
+# first_name = "Giovanna"
+# last_name = "Rocha"
+
+# print(first_name +" "+last_name)
+
+#----------------------------------------------------------------------------------
+
+# number_int = 10 #numero inteiro
+# number_float = 10.5 # numero flutuante
+# name_str = "Hello Word" # string texto
+# is_bool = True # booleano - valores False or True
+# print(number_int)
+# print(number_float)
+# print(name_str)'
+# print(is_bool)
+
+# #---------------------------------------------------------------------------------
+# course_name = "Python Programing"
+
+# print(len(course_name)) # len():  mostra a quantidade de caracteres contando o espaço 
+# print(course_name[0]) # [] : acessa o caractere na posição 0
+# print(course_name[-1]) # [] : acessa o caractere na posição - 1 ou seja, o ultimo caractere da minha string
+# print(course_name[0:3]) # [:] : acessa um intervalo de caracteres
+# print(course_name[3:]) # [:] : acessa um intervalo de caracteres
+
+
+#python é uma linguagem case sensitive - diferença entre maisculas e minusculas
+
+#-----------------------------------------------------------------------------------
+
+# #caracteres de escape 
+
+# course_name = "Python \"Programing\""
+# course_name = "Python \n"Programing\""
+
+# print(course_name)
+
+#-----------------------------------------------------------------------------------
+
+#Concatenação
+
+# first_name = "Gio"
+# last_name = "Rocha"
+# full_name = first_name + " " + last_name
+# full_name2 = f"{first_name} {last_name}"
+
+# print(full_name)
+# print(full_name2)
+
+#-----------------------------------------------------------------------------------
+
+# course = " python programing "
+# course2 = " python programing "
+
+#  print(course2.title()) #maiusculas no inicio do titulo
+#  print(course.upper()) #MAISUCULAS
+#  print(course.lower()) #minusculas
+#  print(course.strip()) #remove os espaços em branco em cada lado da string
+#  print(course.lstrip()) #remove os espaços em branco apenas do lado esquerdo
+#  print(course.rsplit()) #remove os espaços em branco apenas do lado direito
+#  print(course.find("p")) #retorna o indice da primeira ocorrencia
+#  print(course.replace("python", "Django"))# retorna a string com o texto substituido
+#  print(course.replace("p", "j")) 
+#  print("pro" in course) #retorna True ou False
+
+
+#  modified_str = course.upper() #modifica e salva a nova forma da str
+#  print(modified_str)
